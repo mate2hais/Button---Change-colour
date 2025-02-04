5af6b982665c118cb7dea9b451d2e483234c3a08
mate2hais <matei.adrian694@yahoo.com>
2025-02-04 21:05:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-let clickcounter = 0;
 function changeColour() {
-  console.log(document.getElementById('myButton').innerHTML);
-    document.getElementById('myButton').innerHTML
-      ++clickcounter; 
-    if(clickcounter == 1) {
-      document.getElementById('myButton').style.backgroundColor="yellow";
-    } else if (clickcounter == 2) {
-      document.getElementById('myButton').style.backgroundColor="green";
-    } else {
-      clickcounter = 0;
-      document.getElementById('myButton').style.backgroundColor="red";
-    }
-}
+  var button = document.getElementById('myButton'),
+    colors = ['yellow', 'green', 'red'];
+    button.onclick = function () {
+    let  color = colors.shift();
+    colors.push(color);
+    button.style.backgroundColor = color;
+};
